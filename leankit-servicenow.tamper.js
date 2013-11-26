b73d806e2da81661638e378362f330139e4cca08
Toby Murray <toby@ksu.edu>
2013-11-26 11:04:30 -0600

    Adding another regex to linkify bugzilla bugs

--- leankit-servicenow.tamper.js
+++ leankit-servicenow.tamper.js
@@ -13,6 +13,7 @@ setInterval(function(){
       label = $(this).text().replace(
           /((RITM|REQ|INC|TASK)[0-9]+)/,
           '<a target="_" style="color:blue" href="https://kstate.service-now.com/nav_to.do?uri=textsearch.do?sysparm_search=$1">$1</a>');
+      label = label.replace(/(Bug )([0-9]+)/i,'<a target="_" style="color:blue" href="https://tools.ome.ksu.edu/bugzilla/show_bug.cgi?id=$2">$1$2</a>');
       $(this).html(label);
       $(this).addClass("wserewrite");
     });
