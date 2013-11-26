38ee5520cc24048704fb26e11d982b5ebb15705f
Toby Murray <toby@ksu.edu>
2013-11-26 11:28:32 -0600

    Bumping version number since I changed things

--- leankit-servicenow.tamper.js
+++ leankit-servicenow.tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           ServiceNow Leankit Link Replacer
 // @namespace      https://github.com/jeremiahshirk/userscript
-// @version        0.0.3
+// @version        0.0.4
 // @description    replace servicenow identifiers in LeanKit with a link
 // @include        https://ksu.leankit.com/*
 // @run-at         document-end
