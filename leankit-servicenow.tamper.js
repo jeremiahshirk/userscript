4fb922c7a9b7787b2c521811c164e1d37c4046f5
Don Eisele <ksuquix@gmail.com>
2015-07-14 09:34:47 -0500

    added stoppropagation

--- leankit-servicenow.tamper.js
+++ leankit-servicenow.tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           ServiceNow Leankit Link Replacer
 // @namespace      https://github.com/kstateome/userscript
-// @version        0.0.6
+// @version        0.0.7
 // @description    replace servicenow identifiers in LeanKit with a link
 // @include        https://ksu.leankit.com/*
 // @run-at         document-end
@@ -12,7 +12,7 @@ setInterval(function(){
     .each(function () {
       label = $(this).text().replace(
           /((PRB|RITM|REQ|INC|TASK)[0-9]+)/,
-          '<a target="_" style="color:blue" href="https://kstate.service-now.com/nav_to.do?uri=textsearch.do?sysparm_search=$1">$1</a>');
+          '<a target="_" style="color:blue" href="https://kstate.service-now.com/nav_to.do?uri=textsearch.do?sysparm_search=$1" onclick="event.stopPropagation();">$1</a>');
       label = label.replace(/(Bug )([0-9]+)/i,'<a target="_" style="color:blue" href="https://tools.ome.ksu.edu/bugzilla/show_bug.cgi?id=$2">$1$2</a>');
       $(this).html(label);
       $(this).addClass("wserewrite");
