32beeb08cfa8ab520cae1409cf7d0cec7160321c
Don Eisele <ksuquix@gmail.com>
2018-05-10 13:34:06 -0500

    rusty skills

--- sharepoint-doc.tamper.js
+++ sharepoint-doc.tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           EST Sharepoint documentation updates
 // @namespace      https://github.com/kstateome/userscript
-// @version        0.0.1
+// @version        0.0.2
 // @description    automate some sharepoint things
 // @include        https://ksuemailprod.sharepoint.com/sites/est/*
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
@@ -10,7 +10,9 @@
 // ==/UserScript==
 
 $().ready(
+  function() {
     if(window.location.pathname.indexOf('Pages/Forms/AllItems.aspx?id=')>0) {
 	$("a:contains('Overview.aspx')").click();
     }
+  }
 );
