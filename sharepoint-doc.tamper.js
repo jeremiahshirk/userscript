67bf1933ceab7a762a077024675f7e994330754e
Don Eisele <ksuquix@gmail.com>
2018-05-10 13:27:54 -0500

    initial version for sharepoint documentation tamper

---
+++ sharepoint-doc.tamper.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           EST Sharepoint documentation updates
+// @namespace      https://github.com/kstateome/userscript
+// @version        0.0.1
+// @description    automate some sharepoint things
+// @include        https://ksuemailprod.sharepoint.com/sites/est/*
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @grant          GM_addStyle
+// @run-at         document-end
+// ==/UserScript==
+
+$().ready(
+    if(window.location.pathname.indexOf('Pages/Forms/AllItems.aspx?id=')>0) {
+	$("a:contains('Overview.aspx')").click();
+    }
+);
